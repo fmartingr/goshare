a2a74d4221a90aa2cea078820d0c761d1b529a1c
Felipe Martín <fmartingr@gmail.com>
2016-10-09 10:24:50 +0200

    Fixed Violations

--- main.go
+++ main.go
@@ -17,8 +17,7 @@ import (
 )
 
 var (
-	path = kingpin.Arg("path", "Path to file you want to share").Required().String()
-	MAX_PACKET_SIZE = 1<<15
+	Path = kingpin.Arg("path", "Path to file you want to share").Required().String()
 	configDirs = configdir.New("dictget", "goshare")
 	usr, _ = user.Current()
 )
@@ -92,14 +91,14 @@ func main() {
 	}
 
 	// Check if path exists and if it isn't a directory
-	stat, err := os.Stat(*path)
+	stat, err := os.Stat(*Path)
 	if os.IsNotExist(err) || stat.IsDir() {
-		log.Fatalf("%s not found or it isn't a file\n", *path)
+		log.Fatalf("%s not found or it isn't a file\n", *Path)
 	}
 
 	// Copy file to temp folder with new name
 	newFileName := fmt.Sprintf("%s%s",
-		uuid.NewV4().String(), filepath.Ext(*path))
+		uuid.NewV4().String(), filepath.Ext(*Path))
 
 	// Create ssh connection
 	sshConfig := &ssh.ClientConfig{
@@ -117,7 +116,8 @@ func main() {
 	}
 	defer connection.Close()
 
-	client, err := sftp.NewClient(connection, sftp.MaxPacket(MAX_PACKET_SIZE))
+	MaxPacketSize := 1<<15
+	client, err := sftp.NewClient(connection, sftp.MaxPacket(MaxPacketSize))
 
 	if err != nil {
 		log.Fatal("Unable to start SFTP connection")
@@ -132,7 +132,7 @@ func main() {
 	}
 	defer w.Close()
 
-	srcFile, err := ioutil.ReadFile(*path)
+	srcFile, err := ioutil.ReadFile(*Path)
 	if err != nil {
 		log.Fatal(err)
 	}
